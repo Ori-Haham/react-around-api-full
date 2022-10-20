c847a34184f0375dac56fca1a9a6584d76fbd61b
Ori-Haham <ori.haham100@gmail.com>
2022-10-20 14:10:09 +0300

    backend 1

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -13,7 +13,7 @@ module.exports.postNewCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((user) => {
-      res.send({ data: name, link });
+      res.send({ data: name });
     })
     .catch(next);
 };
