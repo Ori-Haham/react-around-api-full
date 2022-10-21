b122f7f366b9a2a3c9c56b232e280abb5b588289
Ori-Haham <ori.haham100@gmail.com>
2022-10-21 11:16:10 +0300

    posr card error

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -1,46 +1,48 @@
 const Card = require('../models/card');
+const NotFoundError = require('../errors/notFoundErr');
 const errorHandler = require('../error/errorHandler');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => {
-      errorHandler(err, req, res);
+      res.send('ori');
     });
 };
 
 module.exports.postNewCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({ name, link, owner: req.user._id })
+  Card.create({ name, link, owner: req.user._id }, function (error) {
+    console.log(error);
+  })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       errorHandler(err, req, res);
     });
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.cardId)
     .orFail(() => {
-      const error = new Error('No card found with that id');
-      error.statusCode = 404;
-      throw error;
+      throw new NotFoundError('No user with matching ID found');
     })
     .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      errorHandler(err, req, res);
-    });
+    .catch(next);
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
-    .then((user) => res.send({ data: 'ori' }))
-    .catch((err) => {
-      errorHandler(err, req, res);
-    });
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('No card with matching ID found');
+      }
+      res.send({ data: user });
+    })
+    .catch(next);
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -49,8 +51,11 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true }
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      errorHandler(err, req, res);
-    });
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('No card with matching ID found');
+      }
+      res.send({ data: user });
+    })
+    .catch(next);
 };
