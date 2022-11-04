1366c07dd0eb72fea55ff307c78814f36d4e1b84
Ori_Haham <ori.haham100@gmail.com>
2022-11-04 13:44:27 +0000

    update 1

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+var cors = require('cors');
 
-const { errors } = require('celebrate');
+const { celebrate, Joi, errors, Segments } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -11,18 +12,24 @@ mongoose.connect('mongodb://localhost:27017/aroundb');
 
 const auth = require('./middleware/auth');
 
+const { userCredentialsValidator } = require('./middleware/userValidators');
 const { postNewUser, login } = require('./controllers/users');
 const usersRoute = require('./routes/users');
 const cardsRoute = require('./routes/cards');
+
+app.use(cors());
+app.options('*', cors());
+
 console.log(1);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.post('/signup', postNewUser);
+app.post('/signup', userCredentialsValidator, postNewUser);
 
-app.post('/signin', login);
+app.post('/signin', userCredentialsValidator, login);
+
 console.log(2);
 app.use(auth);
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi, Segments } = require('celebrate');
+
 const {
   getUsers,
   getUser,
@@ -13,8 +15,32 @@ router.get('/users', getUsers);
 
 router.get('/users/me', getUser);
 
-router.patch('/users/me', updateProfile);
+router.patch(
+  '/users/me',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      about: Joi.string().min(2).max(30).required(),
+    }),
+  }),
+  updateProfile
+);
 
-router.patch('/users/me/avatar', updateAvatar);
+router.patch(
+  '/users/me/avatar',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      avatar: Joi.string()
+        .required()
+        .custom((value, helpers) => {
+          if (validator.isURL(value)) {
+            return value;
+          }
+          return helpers.error('string.uri');
+        }),
+    }),
+  }),
+  updateAvatar
+);
 
 module.exports = router;
