ba90bbebd5052288e8af53c8801fe13ae6629f8e
Ori-Haham <ori.haham100@gmail.com>
2022-11-06 15:03:17 +0200

    updte 2

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-const { userDataValidator } = require('../middleware/userValidators');
-=======
 const { celebrate, Joi, Segments } = require('celebrate');
->>>>>>> de83f149635fad1b97e09d8813d2ac13d9ade748
+
+const {
+  updateUserDataVlidator,
+  avatarValidator,
+} = require('../middleware/userValidators');
 
 const {
   getUsers,
@@ -11,58 +12,13 @@ const {
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
-console.log(getUsers,
-  getUser,
-  updateProfile,
-  updateAvatar);
+
 router.get('/users', getUsers);
 
 router.get('/users/me', getUser);
-<<<<<<< HEAD
 
-// router.get('/users/:id', getUserById);
+router.patch('/users/me', updateUserDataVlidator, updateProfile);
 
-router.patch(
-  '/users/me',
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      name: Joi.string().min(2).max(30).required(),
-      about: Joi.string().min(2).max(30).required(),
-    }),
-  }),
-  updateProfile
-);
-
-router.patch(
-=======
-
-router.patch(
-  '/users/me',
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      name: Joi.string().min(2).max(30).required(),
-      about: Joi.string().min(2).max(30).required(),
-    }),
-  }),
-  updateProfile
-);
-
-router.patch(
->>>>>>> de83f149635fad1b97e09d8813d2ac13d9ade748
-  '/users/me/avatar',
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      avatar: Joi.string()
-        .required()
-        .custom((value, helpers) => {
-          if (validator.isURL(value)) {
-            return value;
-          }
-          return helpers.error('string.uri');
-        }),
-    }),
-  }),
-  updateAvatar
-);
+router.patch('/users/me/avatar', avatarValidator, updateAvatar);
 
 module.exports = router;
